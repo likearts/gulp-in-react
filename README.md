fe09b7d94e4150d8964f5034f8bf1a0739ace596
likearts <likearts@gnifrix.com>
2022-05-11 21:12:02 +0900

    init

---
+++ README.md
@@ -0,0 +1,24 @@
+# gulp-simple-example
+Simple but fully functional example of Gulp to perform front-end builds.
+
+## Getting Started
+First install gulp and project dependencies with commands:
+```
+$ npm install -g gulp
+$ npm install
+```
+After perform the created tasks in gulpfile:
+
+To execute all tasks:
+```
+$ gulp
+```
+
+To execute a single task as 'html', 'js', 'css' or 'watch' tasks:
+```
+$ gulp {task name}
+```
+
+The Builds go to the 'dist' folder. 
+
+Enjoy!
---
+++ gulpfile.js
@@ -0,0 +1,107 @@
+const 
+    gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    concat = require('gulp-concat'), 
+    minifycss = require('gulp-minify-css'), 
+    uglify = require('gulp-uglify'), 
+    useref = require('gulp-useref'),
+    path = require('path'),
+    { readDirDeep, readDirDeepSync } = require('read-dir-deep');
+
+var html = {
+  source: 'src',
+  sub: 'react/sub-project/build',
+  target: 'dist',
+  subTarget: 'dist/sub'
+}
+
+var css = {
+  source: 'src/css',
+  sub: 'react/sub-project/build/static/css',
+  target: 'dist/css',
+  subTarget: 'dist/sub/static/css'
+};
+
+var js = {
+  source: 'src/js',
+  sub: 'react/sub-project/build/static/js',
+  target: 'dist/js',
+  subTarget: 'dist/sub/static/js'
+};
+
+var sub = {
+  root: 'sub',
+  source: 'react/sub-project/build/**',
+  target: 'dist/sub'
+};
+
+gulp.task('html', function() {
+  gulp.src([
+    html.source + '/*.html'
+  ])
+  .pipe(useref({noAssets: true}))
+  .pipe(gulp.dest(html.target));
+});
+
+gulp.task('css', function() {
+  gulp.src([
+    css.source + '/*.css'
+  ])
+  .pipe(minifycss())
+  .pipe(concat('all.min.css'))
+  .pipe(gulp.dest(css.target));
+});
+
+gulp.task('js', function() {
+  gulp.src([
+    js.source + '/*.js'
+  ])
+  .pipe(uglify({mangle:true}).on('error', gutil.log))
+  .pipe(concat('all.min.js'))
+  .pipe(gulp.dest(js.target));
+});
+
+gulp.task('sub', function() {
+  gulp.src([
+    sub.source + '/*.*',
+  ])
+  .pipe(gulp.dest(sub.target));
+});
+
+/*
+gulp.task('subCss', function() {
+  gulp.src([
+    sub.source + '/*.*'
+  ])
+  .pipe(minifycss())
+  .pipe(gulp.dest(css.subTarget));
+});
+
+gulp.task('subJs', function() {
+  gulp.src([
+    js.sub + '/*.js'
+  ])
+  .pipe(uglify({mangle:true}).on('error', gutil.log))
+  .pipe(gulp.dest(js.subTarget));
+});
+
+gulp.task('subAssets', function() {
+  gulp.src([
+    js.sub + '/*.*'
+  ])
+  .pipe(uglify({mangle:true}).on('error', gutil.log))
+  .pipe(gulp.dest(js.subTarget));
+});
+*/
+
+
+gulp.task('default', ['html', 'css', 'js', 'sub']);
+
+gulp.task('watch', function() {
+  gulp.watch(html.source + '/*.html', ['html']);
+  gulp.watch(css.source + '/*.css', ['css']);
+  gulp.watch(js.source + '/*.js', ['js']);
+  gulp.watch(html.sub + '/*.html', ['subHtml']);
+  gulp.watch(css.sub + '/*.css', ['subCss']);
+  gulp.watch(js.sub + '/*.js', ['subJs']);
+});
